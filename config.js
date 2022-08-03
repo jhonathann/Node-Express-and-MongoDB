216d69130928f9a757cf95f28051087ce7d76fe5
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2022-08-03 11:25:24 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "5555",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "1482635228842998",
+    clientSecret: "3dc8297184ddd76135b793a419a9ee61",
+  },
 };
